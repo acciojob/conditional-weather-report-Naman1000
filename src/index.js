ff0ba87bdf1aa7a9cd0eda70665238aa12cd7576
Naman1000 <91007344+Naman1000@users.noreply.github.com>
2025-01-04 19:33:31 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
+const WeatherDisplay = ({ weather }) => {
+  const { temperature, conditions } = weather;
+
+  const tempStyle = {
+    color: temperature > 20 ? "red" : "blue",
+    fontWeight: "bold",
+  };
+
+  return (
+    <div>
+      <p style={tempStyle}>Temperature: {temperature}</p>
+      <span>Conditions: {conditions}</span>
+    </div>
+  );
+};
+
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
