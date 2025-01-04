4d154c277ccfbe21e0226cae91284daaed586508
Naman1000 <91007344+Naman1000@users.noreply.github.com>
2025-01-04 21:50:19 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,22 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-const WeatherDisplay = ({ weather }) => {
-  const { temperature, conditions } = weather;
 
-  const tempStyle = {
-    color: temperature > 20 ? "red" : "blue",
-    fontWeight: "bold",
-  };
-
-  return (
-    <div>
-      <p style={tempStyle}>Temperature: {temperature}</p>
-      <p>Conditions: {conditions}</p>
-    </div>
-  );
-};
-export default WeatherDisplay;
 
 
 
