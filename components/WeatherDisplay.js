a8c209559eead9a80090cc0eb9273d8a9893f942
Naman1000 <91007344+Naman1000@users.noreply.github.com>
2025-01-04 21:49:16 +0530

    Update WeatherDisplay.js

--- components/WeatherDisplay.js
+++ components/WeatherDisplay.js
@@ -1,1 +1,19 @@
+import React from "react";
 
+const WeatherDisplay = ({ weather }) => {
+  const { temperature, conditions } = weather;
+
+  const tempStyle = {
+    color: temperature > 20 ? "red" : "blue",
+    fontWeight: "bold",
+  };
+
+  return (
+    <div>
+      <p style={tempStyle}>Temperature: {temperature}</p>
+      <p>Conditions: {conditions}</p>
+    </div>
+  );
+};
+
+export default WeatherDisplay;
